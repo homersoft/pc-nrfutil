9acc4297317eaa27ab5a16cb7d2a3c955176b325
Przemysław Fierek <przemyslaw.fierek@silvair.com>
2018-07-17 14:33:48 +0200

    SS-3867: Add doc strings.

--- nordicsemi/dfu/bluez.py
+++ nordicsemi/dfu/bluez.py
@@ -27,6 +27,11 @@ BLUEZ_DEV_IF = "org.bluez.Device1"
 
 
 def uuid_to_ble_uuid(uuid):
+  """ Function converts UUID to BLEUUID.
+
+  :param uuid: uuid.UUID
+  :return: BLEUUID
+  """
   uuid = UUID(uuid)
 
   uuid_bytes = bytearray(uuid.bytes)
@@ -39,6 +44,11 @@ def uuid_to_ble_uuid(uuid):
 
 
 def ble_uuid_to_uuid(ble_uuid):
+  """ Function converts BLEUUID to UUID.
+
+  :param ble_uuid: BLEUUID
+  :return: uuid.UUID
+  """
   uuid_bytes = bytearray(ble_uuid.base.base)
 
   uuid_bytes[2] = (ble_uuid.value >> 8) & 0xFF
@@ -48,25 +58,51 @@ def ble_uuid_to_uuid(ble_uuid):
 
 
 def get_bluez_objects(bus):
+  """ Function gets all DBUS objects from org.bluez.
+
+  :param bus: path to bus.
+
+  :return: dbus.Dictionary<dbus.String, dbus.Dictionary>, paths to objects
+  """
   obj = bus.get_object("org.bluez", "/")
   obj_mgr_if = dbus.Interface(obj, DBUS_OBJECT_MANAGER_IF)
 
   return obj_mgr_if.GetManagedObjects()
 
 
-def find_interfaces_with_name(interfaces, name):
-  return [interface_name for interface_name in interfaces.keys() if interface_name == name]
+def contains_interfaces_with_name(interfaces, name):
+  """ Function checks if interfaces contains interface with name.
+
+  :param interfaces: dbus.Dictionary<dbus.String, dbus.Dictionary>, interfaces
+  :param name: str, interface name
+
+  :return: list<interfaces>
+  """
+  return any([interface_name for interface_name in interfaces.keys() if interface_name == name])
 
 
 def find_ble_adapters(objects):
-  return [path for path, interfaces in objects.items() if any(find_interfaces_with_name(interfaces, BLUEZ_ADAPTER_IF))]
+  """ Function finds all bluetooth adapter compatible with BlueZ.
+
+  :param objects: dbus.Dictionary<dbus.String, dbus.Dictionary>
+
+  :return: list<dbus.String>, list of DBUS paths to adapters.
+  """
+  return [path for path, interfaces in objects.items() if contains_interfaces_with_name(interfaces, BLUEZ_ADAPTER_IF)]
 
 
 def find_ble_devices(objects):
-  return [path for path, interfaces in objects.items() if any(find_interfaces_with_name(interfaces, BLUEZ_DEV_IF))]
+  """ Function finds all devices detected by an adapter.
+
+  :param objects: dbus.Dictionary<dbus.String, dbus.Dictionary>
+
+  :return: list<dbus.String>, list of DBUS paths to adapters.
+  """
+  return [path for path, interfaces in objects.items() if contains_interfaces_with_name(interfaces, BLUEZ_DEV_IF)]
 
 
 class BleGattCharacteristic(object):
+  """ Wrapper that simplify DBUS characteristic API. """
 
   def __init__(self, bus, char_path):
     self._bus = bus
@@ -79,9 +115,18 @@ class BleGattCharacteristic(object):
     self._notification_handlers = []
 
   def add_notification_handler(self, handler):
+    """ Add notification handler.
+
+    :param handler: Callable, handler to be called when notification received.
+    """
     self._notification_handlers.append(handler)
 
   def write(self, data, offset=0):
+    """ Write data to characteristic.
+
+    :param data: bytearray, data to be written
+    :param offset: int, offset
+    """
     options = dbus.types.Dictionary()
     options["offset"] = offset
 
@@ -99,10 +144,12 @@ class BleGattCharacteristic(object):
         handler(self, bytearray(new_value))
 
   def start_notify(self):
+    """ Enable notifications. """
     self._dbus_prop_if.connect_to_signal("PropertiesChanged", self.notification_handler)
     self._dbus_if.StartNotify()
 
   def stop_notify(self):
+    """ Disable notifications. """
     self._dbus_if.StopNotify()
 
   @property
@@ -118,6 +165,7 @@ class BleGattCharacteristic(object):
 
 
 class BleDevice(object):
+  """ Wrapper that simplify DBUS device API. """
 
   DEFAULT_RSSI = -100
   DEFAULT_NAME = "N/A"
@@ -133,6 +181,7 @@ class BleDevice(object):
     self._characteristics = []
 
   def connect(self):
+    """ Connect to device. """
     try:
       self._dbus_if.Connect()
       return True
@@ -141,6 +190,7 @@ class BleDevice(object):
       return False
 
   def disconnect(self):
+    """ Disconnect from device. """
     try:
       self._dbus_if.Disconnect()
       return True
@@ -149,6 +199,7 @@ class BleDevice(object):
       return False
 
   def discover_services(self):
+    """ Perform service discovery procedure. """
     for path, interfaces in get_bluez_objects(self._bus).items():
       if not self._device_path in path:
         continue
@@ -193,6 +244,7 @@ class BleDevice(object):
 
 
 class BleAdapter(object):
+  """ Wrapper that simplify DBUS adapter API. """
 
   def __init__(self, bus, adapter_path):
     """
@@ -204,14 +256,23 @@ class BleAdapter(object):
     self._dbus_prop_if = dbus.Interface(self._dbus_obj, DBUS_PROPERTIES_IF)
 
   def remove_device(self, device_path):
+    """ Remove device object.
+
+    :param device_path: str, path to the device to be removed.
+    """
     self._dbus_if.RemoveDevice(device_path)
 
   def clear_last_discovery_results(self):
+    """ Clear last discovery result. """
     for dev in self.devices():
       dev.disconnect()
       self.remove_device(dev.device_path)
 
   def start_discovery(self):
+    """ Start discovery procedure.
+
+    :return: bool, True when startted, False otherwise
+    """
     self.clear_last_discovery_results()
 
     try:
@@ -222,8 +283,11 @@ class BleAdapter(object):
       logger.error("%s: %s", err.get_dbus_name(), err.get_dbus_message())
       return False
 
-
   def stop_discovery(self):
+    """ Stop discovery procedure.
+
+    :return: bool, True when stopped, False otherwise
+    """
     try:
       self._dbus_if.StopDiscovery()
       return True
@@ -233,10 +297,18 @@ class BleAdapter(object):
       return False
 
   def devices(self):
+    """ Return found devices.
+
+    :return: list<BleDevice>, list of BLE devices.
+    """
     return [BleDevice(self._bus, dev_path) for dev_path in find_ble_devices(get_bluez_objects(self._bus))]
 
   @classmethod
   def first_adapter(cls):
+    """ Create adapter instance with first found BLE adapter.
+
+    :return: cls, adapter instance
+    """
     bus = dbus.SystemBus()
 
     available_adapters = find_ble_adapters(get_bluez_objects(bus))
@@ -248,20 +320,32 @@ class BleAdapter(object):
 
 
 class BluezConnection(object):
+  """ Class keeps information related to connection. """
 
   def __init__(self, conn_handle, device):
     self.conn_handle = conn_handle
     self.device = device
 
   def discover_services(self):
+    """ Perform service discovery procedure. """
     self.device.discover_services()
 
   def get_char_handle(self, uuid):
+    """ Get characteristic.
+
+    :param uuid: BLEUUID, characteristic UUID
+
+    :return: BleCharacteristic or None
+    """
     for char in self.device.characteristics:
       if str(char.uuid) == str(ble_uuid_to_uuid(uuid)):
         return char
 
   def get_cccd_handle(self, uuid):
+    """ Get CCCD. This is dummy method always return None.
+
+    :return: None
+    """
     # This method is not implemented because buttonless bootloader is not supported in firmware.
     return None
 
@@ -287,6 +371,7 @@ class BluezConnectionManager(object):
 
 
 class BluezDriver(object):
+  """ Class is responsible for calling low level operations on BlueZ stack. """
 
   def __init__(self):
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
@@ -305,10 +390,14 @@ class BluezDriver(object):
     self.conn_manager = BluezConnectionManager()
 
   def __main_loop(self):
+    """ Thread with main loop.
+    This main loop is required by dbus python library to be able to receive DBUS signals. It just run in loop.
+    """
     loop = GLib.MainLoop()
     loop.run()
 
   def __device_scanner(self):
+    """ Thread with device scanner."""
     self._scanning_in_progress_evt.set()
 
     while self._scanning_in_progress_evt.is_set():
@@ -333,15 +422,25 @@ class BluezDriver(object):
       time.sleep(0.2)
 
   def observer_register(self, observer):
+    """ Register observer.
+
+    :param observer: BLEDriverObserver
+    """
     self.observers.append(observer)
 
   def observer_unregister(self, observer):
+    """ Unregister observer.
+
+    :param observer: BLEDriverObserver
+    """
     self.observers.remove(observer)
 
   def open(self):
+    """ Open driver."""
     self.__start_main_thread()
 
   def close(self):
+    """ Close driver. """
     self.__stop_scanner_thread()
     self.__stop_main_thread()
 
@@ -354,14 +453,23 @@ class BluezDriver(object):
     pass
 
   def ble_gap_scan_start(self):
+    """ Start scanning for devices. """
     self._ble_adapter.start_discovery()
     self.__start_scanner_thread()
 
   def ble_gap_scan_stop(self):
+    """ Stop scanning for devices. """
     self.__stop_scanner_thread()
     self._ble_adapter.stop_discovery()
 
   def ble_gap_connect(self, address, scan_params, conn_params):
+    """ Connect to device.
+    It also notifies registered observers when connection is successful.
+
+    :param address: BLEGapAddr, device address
+    :param scan_params: None, scan parameters 
+    :param conn_params: BLEGapConnParams, connection parameters
+    """
     self._scanning_in_progress_evt.clear()
 
     dev = self.devices[binascii.hexlify(address.addr)]
@@ -381,6 +489,10 @@ class BluezDriver(object):
                                conn_params    = conn_params)
 
   def ble_gap_disconnect(self, conn_handle):
+    """ Disconnect from device.
+
+    :param conn_handle:, int, connection handle.
+    """
     conn = self.conn_manager.get_connection(conn_handle)
     if conn is None:
       logger.info("Could not disconnect. Connection does not exist.")
@@ -392,9 +504,14 @@ class BluezDriver(object):
       obs.on_gap_evt_disconnected(self, conn_handle, BLEHci.success)
 
   def get_connection(self, conn_handle):
+    """ Get connection by connection handle.
+
+    :param conn_handle: int, connection handle
+    """
     return self.conn_manager.get_connection(conn_handle)
 
   def __start_scanner_thread(self):
+    """ Start scanner thread. """
     if self._scanner_thread is not None:
       raise ValueError("Could not start thread. Looks like other scanner thread has not been finished.")
 
@@ -403,6 +520,7 @@ class BluezDriver(object):
     self._scanner_thread.start()
 
   def __stop_scanner_thread(self):
+    """ Stop scanner thread. """
     if self._scanner_thread is None:
       return
 
@@ -416,6 +534,7 @@ class BluezDriver(object):
     self._scanner_thread = None
 
   def __start_main_thread(self):
+    """ Start main thread. """
     if self._main_loop_thread is not None:
       raise ValueError("Could not start thread. Main loop thread has been already started.")
 
@@ -424,6 +543,7 @@ class BluezDriver(object):
     self._main_loop_thread.start()
 
   def __stop_main_thread(self):
+    """ Stop main thread. """
     if self._main_loop_thread is None:
       return
 
@@ -435,6 +555,7 @@ class BluezDriver(object):
 
 
 class BluezBleAdapter(object):
+  """ Class responsible for interfaction with nrf-util. It is kind of adapter for BluezDriver. """
 
   DEFAULT_ATT_MTU = 23
 
@@ -446,24 +567,40 @@ class BluezBleAdapter(object):
     self.driver = BluezDriver()
 
   def observer_register(self, observer):
+    """ Register observer.
+
+    :param observer: BLEAdapterObserver, observer
+    """
     self.observers.append(observer)
 
   def observer_unregister(self, observer):
+    """ Unregister observer.
+
+    :param observer: BLEAdapterObserver, observer
+    """
     self.observers.remove(observer)
 
   def open(self):
+    """ Open adapter. """
     self.driver.open()
 
     self.conn_in_progress = False
     self.db_conns = dict()
 
   def close(self):
+    """ Close adapter. """
     self.driver.close()
 
     self.conn_in_progress = False
     self.db_conns = dict()
 
   def connect(self, address, scan_params=None, conn_params=None):
+    """Connect to device.
+
+    :param address: BLEGapAddr, device address
+    :param scan_params: None, scan params
+    :param conn_params: BLEGapConnParams, conn params
+    """
     if self.conn_in_progress:
         return
 
@@ -473,21 +610,45 @@ class BluezBleAdapter(object):
                                 conn_params = conn_params)
 
   def disconnect(self, conn_handle):
+    """ Disconnect from device.
+
+    :param conn_handle: int, connection handle
+    """
     self.driver.ble_gap_disconnect(conn_handle)
 
   def att_mtu_exchange(self, conn_handle):
+    """ Set ATT MTU.
+    Current implementation does not support change of ATT MTU.
+
+    :param conn_handle: int, connection handle
+
+    :return: int, ATT MTU
+    """
     # Currently changing ATT MTU is not possible.
     self.db_conns[conn_handle].att_mtu = self.DEFAULT_ATT_MTU
     return self.db_conns[conn_handle].att_mtu
 
   @NordicSemiErrorCheck(expected = BLEGattStatusCode.success)
   def service_discovery(self, conn_handle, uuid=None):
+    """ Perform service discovery.
+
+    :param conn_handle: int, connection handle
+    :param uuid: BLEUUID, uuid
+    
+    :return: BLEGattStatusCode, return code
+    """
     self.db_conns[conn_handle] = self._get_connection_by_conn_handle(conn_handle)
     self.db_conns[conn_handle].discover_services()
 
     return BLEGattStatusCode.success
 
   def _get_connection_by_conn_handle(self, conn_handle):
+    """ Get connection.
+
+    :param conn_handle: int, connection handle
+
+    :return: BluezConnection
+    """
     conn = self.driver.get_connection(conn_handle)
     
     if conn is not None:
@@ -496,6 +657,13 @@ class BluezBleAdapter(object):
     raise ValueError("Could not get connection with conn_handle: {}".format(conn_handle))
 
   def _get_characteristic_with_uuid(self, conn, uuid):
+    """ Get characteristic with UUID.
+
+    :param conn: BluezConnection, connection
+    :param uuid: BLEUUID, uuid
+
+    :return: BleCharacteristic
+    """
     char = conn.get_char_handle(uuid)
 
     if char is not None:
@@ -504,11 +672,24 @@ class BluezBleAdapter(object):
     raise ValueError("Could not get characteristic with UUID: {}".format(str(uuid)))
 
   def _on_notification_handler(self, char, data, conn_handle):
+    """ Handler called on characteristic notification.
+
+    :param char: BleCharacteristic, characteristic
+    :param data: bytearray, received data
+    :param conn_handle: int, connection handle
+    """
     for obs in self.observers:
       obs.on_notification(self, conn_handle, uuid_to_ble_uuid(char.uuid), data)
 
   @NordicSemiErrorCheck(expected=BLEGattStatusCode.success)
   def enable_notification(self, conn_handle, uuid):
+    """ Enable notifications for characteristic with UUID.
+
+    :param conn_handle: int, connection handle
+    :param uuid: BLEUUID, uuid
+
+    :return: BLEGattStatusCode
+    """
     char = self._get_characteristic_with_uuid(self._get_connection_by_conn_handle(conn_handle), uuid)
 
     char.add_notification_handler(functools.partial(self._on_notification_handler, conn_handle=conn_handle))
@@ -518,6 +699,13 @@ class BluezBleAdapter(object):
 
   @NordicSemiErrorCheck(expected=BLEGattStatusCode.success)
   def disable_notification(self, conn_handle, uuid):
+    """ Disable  notifications for characteristic with UUID.
+
+    :param conn_handle: int, connection handle
+    :param uuid: BLEUUID, uuid
+
+    :return: BLEGattStatusCode
+    """
     char = self._get_characteristic_with_uuid(self._get_connection_by_conn_handle(conn_handle), uuid)
     char.stop_notify()
 
@@ -525,6 +713,14 @@ class BluezBleAdapter(object):
 
   @NordicSemiErrorCheck(expected=BLEGattStatusCode.success)
   def write_req(self, conn_handle, uuid, data):
+    """ Write request to characteristic.
+
+    :param conn_handle: int, connection handle
+    :param uuid: BLEUUID, uuid
+    :param data: list<int>, data to be written
+
+    :return: BLEGattStatusCode
+    """
     char = self._get_characteristic_with_uuid(self._get_connection_by_conn_handle(conn_handle), uuid)
     char.write(data)
 
@@ -532,6 +728,14 @@ class BluezBleAdapter(object):
 
   @NordicSemiErrorCheck(expected=BLEGattStatusCode.success)
   def write_cmd(self, conn_handle, uuid, data):
+    """ Write command to characteristic.
+
+    :param conn_handle: int, connection handle
+    :param uuid: BLEUUID, uuid
+    :param data: list<int>, data to be written
+
+    :return: BLEGattStatusCode
+    """
     char = self._get_characteristic_with_uuid(self._get_connection_by_conn_handle(conn_handle), uuid)
     char.write(data)
 
