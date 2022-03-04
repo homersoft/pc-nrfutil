6923957e2561a99cd0fe060bb1cae2333ce7b1fa
tomaszbiel-silvair <tomasz.biel@silvair.com>
2022-03-04 15:06:29 +0100

    [SS-8501] fixed version

--- setup.py
+++ setup.py
@@ -136,7 +136,7 @@ class NoseTestCommand(TestCommand):
 
 setup(
     name="nrfutil-bluez",
-    version="6.0.3.dev5rec",
+    version="6.0.3.dev6",
     license="Modified BSD License",
     author="Nordic Semiconductor ASA",
     url="https://github.com/NordicSemiconductor/pc-nrfutil",
